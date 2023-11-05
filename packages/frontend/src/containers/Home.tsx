7282bef88e16d398cbbc6d4a710dd40d076e9c2b
greg9702 <grzegorzb9702@gmail.com>
2023-11-05 12:11:56 +0100

    Added POC search bar

--- packages/frontend/src/containers/Home.tsx
+++ packages/frontend/src/containers/Home.tsx
@@ -1,12 +1,19 @@
+import { useState } from "react";
 import Hello from "./Hello";
 import "./Home.css";
+import { SearchBar } from "../components/SearchBar";
 
 export default function Home() {
+  const [results, setResults] = useState([]);
+
   return (
     <div className="Home">
       <div className="lander">
-        <h1>Scratch</h1>
-        <p className="text-muted">A simple note taking app</p>
+        <div className="App">
+          <div className="search-bar-container">
+            <SearchBar setResults={setResults} />
+          </div>
+        </div>
         <Hello />
       </div>
     </div>
